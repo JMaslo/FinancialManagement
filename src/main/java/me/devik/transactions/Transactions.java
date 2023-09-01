67a6e78ed62c6076c7383ebdf0b779f4a846c124
JMaslo <hry.v@post.cz>
2023-09-01 08:13:21 +0200

    Finished saveTransactions method

--- src/main/java/me/devik/transactions/Transactions.java
+++ src/main/java/me/devik/transactions/Transactions.java
@@ -4,6 +4,8 @@ package me.devik.transactions;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,11 +19,20 @@ public class Transactions {
         System.out.println("Here are your transactions: " + transactions);
     }
 
-    public void writeIntoYaml(long amountMoney, boolean paying, boolean getMoney){
+    public void saveTransaction( long amountMoney, boolean paying, boolean getMoney ){
         Map<String, Object> data = new HashMap<>();
         data.put("amountMoney", amountMoney);
         data.put("paying", paying);
         data.put("getMoney", getMoney);
+
+        try ( FileWriter fileWriter = new FileWriter(file) ){
+
+            yaml.dump(data, fileWriter);
+            System.out.println("Data written in transactions.yaml!");
+
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
     }
 
     // Saving transaction
