6704a7ea23b3f5d4cb758360be13ffd92f2e02e9
JMaslo <hry.v@post.cz>
2023-09-01 08:05:20 +0200

    .

--- src/main/java/me/devik/transactions/Transactions.java
+++ src/main/java/me/devik/transactions/Transactions.java
@@ -4,6 +4,8 @@ package me.devik.transactions;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Transactions {
 
@@ -11,8 +13,15 @@ public class Transactions {
     Yaml yaml = new Yaml();
     String transactions = "It should be in .yaml file!";
 
-    public void printTransactions() {
+    public void printTransactions(){
         System.out.println("Here are your transactions: " + transactions);
+    }
+
+    public void writeIntoYaml(long amountMoney, boolean paying, boolean getMoney){
+        Map<String, Object> data = new HashMap<>();
+        data.put("amountMoney", amountMoney);
+        data.put("paying", paying);
+        data.put("getMoney", getMoney);
     }
 
     // Saving transaction
